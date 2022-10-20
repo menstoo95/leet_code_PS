330ae9e74a0b58d9febf631be9a99bd7345c2bc0
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2022-10-20 15:30:59 +0900

    Time: 35 ms (29.66%), Space: 5.9 MB (74.10%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,17 @@
+#define ll long long
 class Solution {
 public:
     bool isPalindrome(int x) {
         if(x < 0)
             return false;
         
-        string tmp = to_string(x);
+        ll n = x, comp = 0;
         
-        if(tmp.size() == 1)
-            return true;
+        while(n){
+            comp = comp*10 + n%10;
+            n /= 10;
+        }
         
-        for(int i = 0; i < tmp.size()/2; i++)
-            if(tmp[i] != tmp[tmp.size()-1-i])
-                return false;
-        
-        return true;
+        return x == comp;
     }
 };
