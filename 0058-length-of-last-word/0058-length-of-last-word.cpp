646dc917a6227d4dee56ce33b88fe642a40ff36a
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2022-10-20 15:49:13 +0900

    Time: 5 ms (37.31%), Space: 6.5 MB (51.56%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans = 0, flag = 0;
-        for(int i = s.size()-1; i >= 0; i--){
-            if(s[i] == ' ' && flag == 0)
-                continue;
-            else if(s[i] != ' '){
+        // myCode
+        // int ans = 0, flag = 0;
+        // for(int i = s.size()-1; i >= 0; i--){
+        //     if(s[i] == ' ' && flag == 0)
+        //         continue;
+        //     else if(s[i] != ' '){
+        //         ans++;
+        //         flag++;
+        //     }
+        //     else if(s[i] == ' ' && flag)
+        //         break;
+        // }
+        // return ans;
+        
+        int ans = 0, i = s.size()-1;
+        while(i >= 0 && (s[i] != ' ' || !ans)){
+            if(s[i] != ' ')
                 ans++;
-                flag++;
-            }
-            else if(s[i] == ' ' && flag)
-                break;
+            i--;
         }
+        
         return ans;
     }
 };
