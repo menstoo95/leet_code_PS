541a0aa924455ca37289c3eb945a20b3a85014d8
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2022-11-09 11:55:03 +0900

    Time: 2 ms (46.08%), Space: 6 MB (44.39%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string n = to_string(num);
+        
+        for(int i = 0; i < n.size(); i++){
+            if(n[i] == '6'){
+                n[i] = '9';
+                return stoi(n);
+            }
+        }
+        
+        return num;
+    }
+};
