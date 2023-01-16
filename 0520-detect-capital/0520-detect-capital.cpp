837eadcbae656625e32c2940031135d979527f47
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-01-16 15:52:41 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (28.94%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size() == 1)
+            return true;
+        
+        bool all = isupper(word[0]) && isupper(word[1]), none = islower(word[0]), part = isupper(word[0]) && islower(word[1]);
+        
+        cout << all << endl;
+        cout << none << endl;
+        cout << part << endl;
+        
+        for(int i = 1; i < word.size(); i++){
+            if(all && islower(word[i]))
+                return false;
+            if(none && isupper(word[i]))
+                return false;
+            if(part && isupper(word[i]))
+                return false;
+        }
+        
+        return true;
+    }
+};
