8f66d73c7327ab6bd5ff4dd1dfc572e0aa7db279
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-01-23 15:47:04 +0900

    Time: 261 ms (31.44%), Space: 107.6 MB (11.52%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -4,18 +4,14 @@ public:
         long long ans = 0;
         long long zeros = 0;
         for(int n : nums){
-            if(n && zeros != 0){
-                ans += zeros * (zeros + 1) / 2;
-                zeros = 0;
-            }
-            else if(n && !zeros)
-                zeros = 0;
-            else if(!n)
+            if(!n){
                 zeros++;
+                ans+=zeros;
+            }
+            else
+                zeros = 0;
         }
-        
-        ans += zeros * (zeros + 1) / 2;
-        
+                
         return ans;
     }
 };
