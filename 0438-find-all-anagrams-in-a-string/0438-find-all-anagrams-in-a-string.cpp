3c9c85d76b17aa7db84f29c44662864cf1a0cc51
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-02-05 20:31:35 +0900

    Time: 1488 ms (5.01%), Space: 84.4 MB (5.02%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> ans;
-        unordered_map<char,int> hash;
-        for(int i = 0; i < p.length(); i++)
-            hash[p[i]]++;
-
-        int count = hash.size();
-        int win_size = p.length();
-        int l = 0;
-        int r = 0;
-
-        while(r < s.length()){
-            if(hash.find(s[r]) != hash.end()){
-                hash[s[r]]--;
-                if(hash[s[r]] == 0)
-                    count--;
-            }
-			
-            if(r-l+1 < win_size)
-                r++;
-            else if(r-l+1 == win_size){
-                if(count == 0)
-                    ans.push_back(l);
-
-                if(hash.find(s[l]) != hash.end()){
-                    hash[s[l]]++;
-                    if(hash[s[l]]==1)
-                        count++;
-                }
-                l++;
-                r++;
-            }
+    bool check(vector<int> hs1, vector<int> hs2){
+        for(int i = 0; i < hs1.size(); i++){
+            if(hs1[i] != hs2[i])
+                return false;
         }
+        return true;
+    }
+    
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> hs1(26), ans;
+        if(s.size() < p.size())
+            return ans;
+        
+        for(int i = 0; i < p.length(); i++)
+            hs1[p[i]-'a']++;
+
+        for(int i = 0; i <= s.size()-p.size(); i++){
+            vector<int> hs2(26);
+            for(int j = i; j < i+p.size(); j++){
+                hs2[s[j]-'a']++;
+            }
+            if(check(hs1, hs2))
+                ans.push_back(i);
+        }
+	
+        
         return ans;
     }
 };
