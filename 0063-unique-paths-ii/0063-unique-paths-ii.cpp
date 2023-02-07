e9ed5d09fec97f418c20b67fe9da92f3c6652fac
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-02-07 16:45:33 +0900

    Time: 3 ms (80.42%), Space: 8 MB (16.21%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -20,12 +20,6 @@ public:
             }
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cout << dp[i][j] << " ";
-            }
-            cout << endl;
-        }
         return dp[n-1][m-1];
             
     }
