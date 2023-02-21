d5037229fe29dfb657d64282d5936eb6ca2a77d9
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-02-22 01:06:37 +0900

    Time: 28 ms (67.14%), Space: 22.2 MB (99.93%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int> hs;
-        for(auto a : nums)
-            hs[a]++;
+        if(nums.size() == 1)
+            return nums[0];
         
-        for(auto h : hs)
-            if(h.second == 1)
-                return h.first;
+        int l = 0, r = nums.size()-1;
         
-        return 0;
+        while(l < r){
+            int mid = (l+r) / 2;
+            
+            if(nums[mid] == nums[mid^1])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        
+        return nums[l];
     }
 };
