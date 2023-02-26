9abfa68a1c76b48dc4b3f19e75990304e1f98e84
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-02-26 23:33:21 +0900

    Time: 356 ms (5.03%), Space: 7.3 MB (79.78%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[502][502];
+    
+    int minDistance(string s, string p) {
+        int diff = 0;
+        for(int j = 1; j <= p.size(); j++)
+            dp[0][j] = j;
+        for(int i = 1; i <= s.size(); i++)
+            dp[i][0] = i;
+        
+        for(int i = 1; i <= s.size(); i++){
+            for(int j = 1; j <= p.size(); j++){
+                if(s[i-1] == p[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1])) + 1;
+            }
+        }
+        
+        for(int i = 0; i <= s.size(); i++){
+            for(int j = 0; j <= p.size(); j++)
+                cout << dp[i][j] << " ";
+            cout << endl;
+        }
+        
+        return dp[s.size()][p.size()];
+    }
+};
