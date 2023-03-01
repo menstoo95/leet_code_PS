928dac1574694f40b2535ac5d6aef5e944475b06
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-03-01 13:36:11 +0900

    Time: 232 ms (48.13%), Space: 63.1 MB (48.74%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,17 +1,55 @@
 class Solution {
+    // Function to merge two sub-arrays in sorted order.
+    void merge(vector<int> &arr, int left, int mid, int right, vector<int> &tempArr) {
+        int start1 = left;
+        int start2 = mid + 1;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        
+        for (int i = 0; i < n1; i++) {
+            tempArr[start1 + i] = arr[start1 + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            tempArr[start2 + i] = arr[start2 + i];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (tempArr[start1 + i] <= tempArr[start2 + j]) {
+                arr[k] = tempArr[start1 + i];
+                i += 1;
+            } else {
+                arr[k] = tempArr[start2 + j];
+                j += 1;
+            }
+            k += 1;
+        }
+
+        while (i < n1) {
+            arr[k] = tempArr[start1 + i];
+            i += 1;
+            k += 1;
+        }
+        while (j < n2) {
+            arr[k] = tempArr[start2 + j];
+            j += 1;
+            k += 1;
+        }
+    }
+
+    void mergeSort(vector<int> &arr, int left, int right, vector<int> &tempArr) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid, tempArr);
+            mergeSort(arr, mid + 1, right, tempArr);
+            merge(arr, left, mid, right, tempArr);
+        }
+    }
+
 public:
     vector<int> sortArray(vector<int>& nums) {
-        map<int,int> hs;
-        vector<int> ans;
-        for(auto n : nums)
-            hs[n]++;
-        
-        for(auto h : hs){
-            while(h.second--){
-                ans.push_back(h.first);
-            }
-        }
-        
-        return ans;
+        vector<int> temporaryArray(nums.size());
+        mergeSort(nums, 0, nums.size() - 1, temporaryArray);
+        return nums;
     }
 };
