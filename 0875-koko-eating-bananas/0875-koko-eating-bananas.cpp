c919d5b4993d77020266a857681b0b2a2906fc17
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-03-08 13:49:12 +0900

    Time: 51 ms (60.76%), Space: 35.7 MB (6.57%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool find_k(long long k, vector<int> piles, int h)
+    {
+        long long res = 0;
+
+        for (double p : piles)
+            res += ceil(p / k);
+
+        return (res <= h);
+    }
+
+    int minEatingSpeed(vector<int> piles, int h)
+    {
+        sort(piles.begin(), piles.end());
+
+        long long l = 1, r = 1e9;
+
+        while (l < r)
+        {
+            long long mid = (l + r) / 2;
+
+            if (find_k(mid, piles, h))
+            {
+                r = mid;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+};
