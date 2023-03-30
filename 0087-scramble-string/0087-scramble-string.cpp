98a241681c2c1432650fb78717c1d609f3a36155
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-03-30 21:56:11 +0900

    Time: 16 ms (88.21%), Space: 15.3 MB (79.59%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<string,bool> hs;
+    
+    bool isScramble(string s1, string s2) {
+        int n = s1.size();
+        if(n <= 1)
+            return s1[0] == s2[0];
+        if(hs.find(s1+s2) != hs.end())
+            return hs[s1+s2];
+    
+        string tmp1 = s1, tmp2 = s2;
+        sort(tmp1.begin(), tmp1.end());
+        sort(tmp2.begin(), tmp2.end());
+        if(tmp1 != tmp2)
+            return hs[s1+s2] = false;
+        
+        for (int i = 1; i <= n - 1; i++)
+        {
+            if ((isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i)))||(isScramble(s1.substr(0, i), s2.substr(n - i)) && isScramble(s1.substr(i), s2.substr(0, n - i))))
+                return hs[s1+s2] = true;
+        }
+        
+        return hs[s1+s2] = false;
+    }
+};
