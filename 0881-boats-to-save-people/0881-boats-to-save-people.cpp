a018abed1ee2057429a300cdb4b3004a99c00a4d
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-04-04 00:50:40 +0900

    Time: 100 ms (14.82%), Space: 41.9 MB (93.02%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int l = 0, r = people.size()-1, ans = 0;
+        
+        while(l < r){
+            if(people[l] + people[r] <= limit){
+                ans++;
+                l++;
+                r--;
+            }
+            else{
+                r--;
+                ans++;
+            }
+        }
+        
+        return l == r ? ans+1 : ans;
+    }
+};
