0b87a91542a86bb28dd7d8b8a8653c74f394e496
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-04-11 18:54:52 +0900

    Time: 93 ms (48.76%), Space: 27.1 MB (28.68%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        
+        for(auto c : s){
+            if(c == '*' && !st.empty())
+                st.pop();
+            else
+                st.push(c);
+        }
+        
+        string ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
