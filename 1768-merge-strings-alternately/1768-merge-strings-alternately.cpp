b46841c1600e814c087e00c26f39468460f565f1
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-04-18 17:04:05 +0900

    Time: 4 ms (38.78%), Space: 6.5 MB (19.57%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0, j = 0;
+        string ans = "";
+        while(i < word1.size() || j < word2.size()){
+            if(i < word1.size()){
+                ans += word1[i];
+                i++;
+            }
+            if(j < word2.size()){
+                ans += word2[j];
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+};
