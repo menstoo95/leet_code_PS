81f80efdf3cfb6e2b432e6644c8ad1357702d418
menstoo95 <54580722+menstoo95@users.noreply.github.com>
2023-06-09 14:28:54 +0900

    Time: 0 ms (100.00%), Space: 7 MB (14.45%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        
+        for(int i = 0; i < nums.size(); i++){
+            int start = nums[i];
+            
+            while(i+1 < nums.size() && nums[i]+1 == nums[i+1])
+                i++;
+            
+            if(start != nums[i])
+                ans.push_back(to_string(start)+"->"+to_string(nums[i]));
+            else
+                ans.push_back(to_string(start));
+        }
+        
+        return ans;
+    }
+};
